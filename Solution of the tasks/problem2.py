1193a7f68a2510744505cf2a4fce5aeb22f165f5
M-YOUSUF-M <yousufmazumder12@gmail.com>
2024-12-21 21:46:34 +0600

    [added] problem solve 2 demo

---
+++ Solution of the tasks/problem2.py
@@ -0,0 +1,19 @@
+import os
+import google.generativeai as genai
+
+image_path_1 = 'img1.jpg'  # Replace with the actual path to your first image
+image_path_2 = 'img2.jpg' # Replace with the actual path to your second image
+
+
+genai.configure(api_key="AIzaSyDa7I_Yqv5rY1fyd5Kzb5EY4LyUgCgzKTI")
+#Choose a Gemini model.
+model = genai.GenerativeModel(model_name="gemini-1.5-pro")
+
+prompt = "Select a desh Based on the mood of the user and available item list  and select the recipe from the image given"
+def take_input(available_item_list,image_list:list):
+    choice = input("what kind of desh you wanna eat today: ")
+    
+    response = model.generate_content([prompt,available_item_list, image_list, choice])
+    print(response.text)
+
+take_input(["fruit","sugger",""],["img2.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg"])
